b5abbf25744ae0af9f0a1b2ded98a5981be2a742
0xamey <ameydhurgude007@gmail.com>
2022-08-21 23:24:41 +0530

    change

--- test/Multisig.t.sol
+++ test/Multisig.t.sol
@@ -16,6 +16,9 @@ contract MultisigTest is Test {
 
     event Deposit(address indexed sender, uint256 amount);
     event Submit(uint256 indexed txId);
+    event Approve(address indexed owner, uint256 indexed txId);
+    event Revoke(address indexed owner, uint256 indexed txId);
+    event Execute(uint256 indexed txId);
 
     function setUp() public {
         owner1 = address(0x1);
